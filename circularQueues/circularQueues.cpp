618da2e2990fa554c2ab7c44c54ecb1551e03d85
Amandaptr44 <amandaptr86@gmail.com>
2023-06-22 10:50:42 +0700

    menambahkan kondisi jika rear berada di posisi terakhir array

--- circularQueues/circularQueues.cpp
+++ circularQueues/circularQueues.cpp
@@ -3,7 +3,7 @@ using namespace std;
 
 class Queues {
 	int FRONT, REAR, max = 5;
-	int queue_array[5]
+	int queue_array[5];
 
 public:
 	Queues() {
@@ -13,13 +13,13 @@ public:
 
 	void insert() {
 		int num;
-		count << "Enter a number: ";
+		cout << "Enter a number: ";
 		cin >> num;
-		count << endl;
+		cout << endl;
 
 		//Cek apakah antrian penuh
 		if ((FRONT == 0 && REAR == max - 1) || (FRONT == REAR + 1)) {
-			count << "\nQueues overflow\n";
+			cout << "\nQueues overflow\n";
 			return;
 		}
 
@@ -28,5 +28,13 @@ public:
 			FRONT = 0;
 			REAR = 0;
 		}
+		else {
+			//Jika benar REAR berada di posisi terakhir array, kembali ke awal array
+			if (REAR == max - 1)
+				REAR = 0;
+			else
+				REAR = REAR + 1;
+		}
+		queue_array[REAR] = num;
 	}
 };
